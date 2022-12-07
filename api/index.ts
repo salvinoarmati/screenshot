b1669f65827faa56e094814f496453e20fec12cc
Faraaz Nishtar <faraaznishtar@gmail.com>
2022-12-07 14:30:39 -0500

    testing something

--- api/index.ts
+++ api/index.ts
@@ -2,17 +2,30 @@ import { getScreenshot } from "./_lib/puppeteer";
 
 module.exports = async (req, res) => {
   if (!req.query.url) return res.status(400).send("No url query specified.");
-  if (!checkUrl(req.query.url)) return res.status(400).send("Invalid url query specified.");
+  if (!checkUrl(req.query.url))
+    return res.status(400).send("Invalid url query specified.");
   try {
-    const file = await getScreenshot(req.query.url, req.query.width, req.query.height);
+    console.log({ QueryURL: req.query.url });
+    const file = await getScreenshot(
+      req.query.url,
+      req.query.width,
+      req.query.height
+    );
     res.setHeader("Content-Type", "image/png");
-    res.setHeader("Cache-Control", "public, immutable, no-transform, s-maxage=86400, max-age=86400");
+    res.setHeader(
+      "Cache-Control",
+      "public, immutable, no-transform, s-maxage=86400, max-age=86400"
+    );
     res.status(200).end(file);
   } catch (error) {
-    console.error(error)
-    res.status(500).send("The server encountered an error. You may have inputted an invalid query.");
+    console.error(error);
+    res
+      .status(500)
+      .send(
+        "The server encountered an error. You may have inputted an invalid query."
+      );
   }
-}
+};
 
 function checkUrl(string, hostname) {
   var url = "";
